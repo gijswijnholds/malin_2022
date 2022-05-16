d0df4123af5e816a01e2212df759a7ccb10040d3
gijswijnholds <gijswijnholds@gmail.com>
2022-05-16 09:53:11 +0200

    Update of lexicon with VR verbs

--- src/lexicon.py
+++ src/lexicon.py
@@ -5,15 +5,23 @@ _data_dir = "../data"
 _names_path = os.path.join(_data_dir, 'Top_eerste_voornamen_NL_2010.csv')
 _de_noun_path = os.path.join(_data_dir, 'de_personen.txt')
 _het_noun_path = os.path.join(_data_dir, 'het_personen.txt')
+
+_trans_inf_animate_obj_path = os.path.join(_data_dir, 'person_transitive_verbs.txt')
+
 _obj_cv_path = os.path.join(_data_dir, 'obj_control_verbs.txt')
 _sub_cv_path = os.path.join(_data_dir, 'sub_control_verbs.txt')
-_inf_path = os.path.join(_data_dir, 'infinitives.txt')
-_t_inf_path = os.path.join(_data_dir, 'person_transitive_verbs.txt')
+
+_intrans_inf_verbs_path = os.path.join(_data_dir, 'infinitives.txt')
+_verb_raisers_extraposition_path = os.path.join(_data_dir, 'verb_raisers_extraposition.txt')
+_verb_raisers_intrans_path = os.path.join(_data_dir, 'verb_raisers_intrans.txt')
+_verb_raisers_trans_path = os.path.join(_data_dir, 'verb_raisers_trans.txt')
+
+
 _adv_path = os.path.join(_data_dir, 'adverbs.txt')
 _ipp_itv_path = os.path.join(_data_dir, 'ipp_intrans_infinitives.txt')
 _ipp_tv_path = os.path.join(_data_dir, 'ipp_trans_infinitives.txt')
 _ipp_itv_te_path = os.path.join(_data_dir, 'ipp_intrans_infinitives_te.txt')
-_nvg_verbs_path = os.path.join(_data_dir, 'nvg_werkwoorden.txt')
+
 
 
 def _load_plain(path: str) -> list[str]:
@@ -42,36 +50,40 @@ _de_nouns = _load_plain(_de_noun_path)
 _het_nouns = _load_plain(_het_noun_path)
 _obj_control_verbs_present, _obj_control_verbs_inf = _load_control_verbs(_obj_cv_path)
 _sub_control_verbs_present, _sub_control_verbs_inf = _load_control_verbs(_sub_cv_path)
-_infinitive_verbs = _load_plain(_inf_path)
-_transitive_infinitive_verbs = _load_plain(_t_inf_path)
+
+_intrans_inf_verbs = _load_plain(_intrans_inf_verbs_path)
+_trans_inf_verbs_animate_obj = _load_plain(_trans_inf_animate_obj_path)
+_verb_raisers_intrans = _load_plain(_verb_raisers_intrans_path)
+_verb_raisers_trans = _load_plain(_verb_raisers_trans_path)
+
 _adverbs = _load_plain(_adv_path)
 _ipp_intransitive_infinitive_verbs = _load_plain(_ipp_itv_path)
 _ipp_transitive_infinitive_verbs = _load_plain(_ipp_tv_path)
 _ipp_intransitive_infinitive_te_verbs = _load_plain(_ipp_itv_te_path)
-_nvg_verbs = _load_plain(_nvg_verbs_path)
+_verb_raisers_extraposition = _load_plain(_verb_raisers_extraposition_path)
 
 
 # Lexical categories/representatives
 #
-# inf0(d0) --> [d0]. % vertrekken, intransitive infinitive
+# inf0(d0) --> [d0]. % vertrekken, intransitive infinitive -> DONE
 # inf1(d1) --> [d1]. % zeggen, transitive infinitive, inanimate object
-# inf1a(d11) --> [d11]. % ontmoeten, transitive infinitive, animate object
+# inf1a(d11) --> [d11]. % ontmoeten, transitive infinitive, animate object -> DONE
 #
-# ivr0(d2) --> [d2]. % willen, obligatory VR
-# ivr1(d3) --> [d3]. % laten, obligatory VR, direct object is understood subject of vc
-# obj1_(d4) --> [d4]. % haar, animate direct object
+# ivr0(d2) --> [d2]. % willen, obligatory VR -> DONE (verb_raisers_intrans)
+# ivr1(d3) --> [d3]. % laten, obligatory VR, direct object is understood subject of vc -> DONE (verb_raisers_trans)
+# obj1_(d4) --> [d4]. % haar, animate direct object -> DONE
 # obj1(d5) --> [d5]. % iets, inanimate direct object
-# obj2(d10) --> [d10]. % hun, indirect object
+# obj2(d10) --> [d10]. % hun, indirect object -> DONE (only animate)
 #
-# ivr2(d6) --> [d6]. % proberen, VR (also: d7)
-# inf2(d7) --> [d7]. % dreigen, extraposition
-# inf3(d9) --> [d9]. % verzoeken+indirect object, extraposition, indirect object=understood su of vc
-# inf4(d12) --> [d12]. % beloven+indirect object, extraposition, subject=understood su of vc
+# ivr2(d6) --> [d6]. % proberen, VR (also: d7) -> DONE (verb_raisers_extraposition)
+# inf2(d7) --> [d7]. % dreigen, extraposition -> DONE (verb_raisers_extraposition)
+# inf3(d9) --> [d9]. % verzoeken+indirect object, extraposition, indirect object=understood su of vc -> DONE (obj control verbs)
+# inf4(d12) --> [d12]. % beloven+indirect object, extraposition, subject=understood su of vc -> DONE (subj control verbs)
 
 class Lexicon:
     @staticmethod
     def inf0():
-        return list(iter(_))
+        return lambda _: list(iter(_intrans_inf_verbs))
 
     @staticmethod
     def inf1():
@@ -79,31 +91,36 @@ class Lexicon:
 
     @staticmethod
     def inf1a():
-        return list(iter(_))
+        return lambda _: list(iter(_trans_inf_verbs_animate_obj))
 
     @staticmethod
     def ivr0():
-        return list(iter(_))
+        return lambda _: list(iter(_verb_raisers_intrans))
 
     @staticmethod
     def ivr1():
-        return list(iter(_))
+        return lambda _: list(iter(_verb_raisers_trans))
 
     @staticmethod
+    # IVR2 = INF2, for verbs that occur both in verb clusters and in extraposition
     def ivr2():
-        return list(iter(_))
+        return lambda _: list(iter(_verb_raisers_extraposition))
 
     @staticmethod
     def inf2():
-        return list(iter(_))
+        return lambda _: list(iter(_verb_raisers_extraposition))
 
     @staticmethod
     def inf3():
-        return list(iter(_))
+        inf3idict = {Lexicon.obj2()(v) for v in list(iter(_obj_control_verbs_inf))}
+        def f(verb) -> list[str]: return inf3idict[verb]
+        return f
 
     @staticmethod
     def inf4():
-        return list(iter(_))
+        inf4idict = {Lexicon.obj2()(v) for v in list(iter(_obj_control_verbs_inf))}
+        def f(verb) -> list[str]: return inf4idict[verb]
+        return f
 
     @staticmethod
     def obj1a():
@@ -115,7 +132,6 @@ class Lexicon:
         inf1idict = ...
         def f(verb) -> list[str]: return inf1idict[verb]
         return f
-        # return list(iter(_))
 
     @staticmethod
     def obj2():
@@ -123,72 +139,12 @@ class Lexicon:
 
     @staticmethod
     def pref1():
-        return ["hij"]
+        return lambda _: ["hij"]
 
     @staticmethod
     def pref2():
-        return ["zal"]
+        return lambda _: ["zal"]
 
     @staticmethod
     def te():
-        return ["te"]
-
-    # @staticmethod
-    # def female_names():
-    #     return list(iter(_female_names))
-    #
-    # @staticmethod
-    # def male_names():
-    #     return list(iter(_male_names))
-    #
-    # @staticmethod
-    # def de_nouns():
-    #     return list(iter(_de_nouns))
-    #
-    # @staticmethod
-    # def het_nouns():
-    #     return list(iter(_het_nouns))
-    #
-    # @staticmethod
-    # def obj_control_verbs_present():
-    #     return list(iter(_obj_control_verbs_present))
-    #
-    # @staticmethod
-    # def obj_control_verbs_inf():
-    #     return list(iter(_obj_control_verbs_inf))
-    #
-    # @staticmethod
-    # def sub_control_verbs_present():
-    #     return list(iter(_sub_control_verbs_present))
-    #
-    # @staticmethod
-    # def sub_control_verbs_inf():
-    #     return list(iter(_sub_control_verbs_inf))
-    #
-    # @staticmethod
-    # def infinitive_verbs():
-    #     return list(iter(_infinitive_verbs))
-    #
-    # @staticmethod
-    # def vos():
-    #     return list(iter(_transitive_infinitive_verbs))
-    #
-    # @staticmethod
-    # def adverbs():
-    #     return list(iter(_adverbs))
-    #
-    # @staticmethod
-    # def ipp_tvs():
-    #     return list(iter(_ipp_transitive_infinitive_verbs))
-    #
-    # @staticmethod
-    # def ipp_itvs():
-    #     return list(iter(_ipp_intransitive_infinitive_verbs))
-    #
-    # @staticmethod
-    # def ipp_itvs_te():
-    #     return list(iter(_ipp_intransitive_infinitive_te_verbs))
-    #
-    # @staticmethod
-    # def nvg_verbs():
-    #     return list(iter(_nvg_verbs))
+        return lambda _: ["te"]
